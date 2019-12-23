2319124866e2b49c62f1f92ed74868fac192651b
andy <andydavies2020@gmail.com>
2019-12-23 17:36:56 +0200

    minimize: better

--- minimize.sh
+++ minimize.sh
@@ -1,7 +1,17 @@
 #!/bin/sh -eux
 
-echo "==> Disk usage before cleanup"
-df -h
+DISK_USAGE_BEFORE_CLEANUP=$(df -h)
+
+if [ -d "/var/lib/dhcp" ]; then
+    rm /var/lib/dhcp/*
+fi
+
+sudo apt-get -y autoremove --purge
+sudo apt-get -y autoclean
+sudo apt-get -y clean
+
+find /var/cache -type f -exec rm -rf {} \;
+find /var/log/ -name *.log -exec rm -f {} \;
 
 # Whiteout root
 count=$(df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}')
@@ -32,14 +42,13 @@ if [ "x${swapuuid}" != "x" ]; then
     /sbin/mkswap -U "$swapuuid" "$swappart";
 fi
 
-sudo apt-get -y autoremove --purge
-sudo apt-get -y autoclean
-sudo apt-get -y clean
-
-find /var/cache -type f -exec rm -rf {} \;
-find /var/log/ -name *.log -exec rm -f {} \;
+dd if=/dev/zero of=/EMPTY bs=1M  || echo "dd exit code $? is suppressed"
+rm -f /EMPTY
 
 sync;
+
+echo "==> Disk usage before cleanup"
+echo "$DISK_USAGE_BEFORE_CLEANUP"
 
 echo "==> Disk usage after cleanup"
 df -h
