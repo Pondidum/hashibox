82dd43f912b640a84e9a0f6f1e53b5b02e99c032
andy <andy.davies@reaktor.com>
2019-12-28 18:52:42 +0200

    alpine: build

--- build.sh
+++ build.sh
@@ -1,14 +1,17 @@
-#! /bin/bash
+#!/bin/sh
 
-set -e
+set -eu
 
 box_name=hashibox.box
+local_name="local/hashibox"
 
-rm -f $box_name
+export VAGRANT_LIBVIRT_VIRT_SYSPREP_OPERATIONS="defaults,-ssh-userdir,-ssh-hostkeys"
 
-vagrant up $@
+vagrant up
 
-vagrant package --output $box_name
-vagrant box add --force --name local/hashibox $box_name
+sudo rm -f "$box_name"
+sudo vagrant package --output "$box_name" --vagrantfile meta.rb
+sudo chown "$USER:$USER" "$box_name"
 
+vagrant box add --force --name "$local_name" "$box_name"
 vagrant destroy -f
