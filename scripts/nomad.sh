a6d3c9d9694aa41d170f005f0ffc8da3e37287b6
andy <andy.davies@reaktor.com>
2020-03-08 12:00:26 +0200

    nomad: add cni plugins for consulconnect support

--- scripts/nomad.sh
+++ scripts/nomad.sh
@@ -1,18 +1,34 @@
-#!/bin/ash -eu
+#!/bin/bash -eu
+
+echo "==> Installing Nomad"
 
 echo "https://sjc.edge.kernel.org/alpine/edge/testing" | tee -a /etc/apk/repositories
 apk update
-apk add nomad
+apk add nomad ip6tables
 
+echo "    building configuration directories"
 rm -rf /etc/nomad.d
 rm /etc/init.d/nomad
 mv /usr/sbin/nomad /usr/local/bin/nomad
 chmod +xr /usr/local/bin/nomad
 
+binaries=/vagrant/binaries
 src=/vagrant/nomad
 
+echo "    adding default configuration"
 mkdir -p /etc/nomad
 
 cp $src/nomad.hcl /etc/nomad/nomad.hcl
 cp $src/nomad.initd /etc/init.d/nomad
 cp $src/nomad.confd /etc/conf.d/nomad
+
+echo  "    installing CNI plugins"
+
+mkdir -p /opt/cni/bin
+tar -C /opt/cni/bin -xzf $binaries/cni-plugins.tgz
+
+echo 1 > /proc/sys/net/bridge/bridge-nf-call-arptables
+echo 1 > /proc/sys/net/bridge/bridge-nf-call-ip6tables
+echo 1 > /proc/sys/net/bridge/bridge-nf-call-iptables
+
+echo "==> Done"
--- src/fetch.sh
+++ src/fetch.sh
@@ -1,28 +1,33 @@
-#! /bin/bash
+#!/bin/bash -eu
 
 CONSUL_VERSION=1.6.2
 VAULT_VERSION=1.3.1
 YQ_VERSION=2.4.1
+CNI_VERSION=0.8.5
 
 OUTPUT_DIR="./src/binaries"
 
 mkdir -p $OUTPUT_DIR
+cd $OUTPUT_DIR
 
-pushd $OUTPUT_DIR
+echo "==> Fetching binaries to $OUTPUT_DIR"
 
-echo "fetching Consul..."
+echo "    Consul $CONSUL_VERSION..."
 curl -sSL https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip -o consul.zip
 
-echo "fetching Vault..."
+echo "    Vault $VAULT_VERSION"
 curl -sSL https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip -o vault.zip
 
-echo "fetching yq..."
+echo "    yq $YQ_VERSION"
 curl -sSL https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64 -o yq
 
-for bin in cfssl cfssl-certinfo cfssljson
-do
-    echo "fetching ${bin}..."
-    curl -sSL https://pkg.cfssl.org/R1.2/${bin}_linux-amd64 > ${bin}
+echo "    CNI Plugins $CNI_VERSION"
+curl -sSL https://github.com/containernetworking/plugins/releases/download/v${CNI_VERSION}/cni-plugins-linux-amd64-v${CNI_VERSION}.tgz -o cni-plugins.tgz
+
+
+for bin in cfssl cfssl-certinfo cfssljson; do
+  echo "    ${bin}"
+  curl -sSL https://pkg.cfssl.org/R1.2/${bin}_linux-amd64 > ${bin}
 done
 
-popd
+echo "==> Done"
